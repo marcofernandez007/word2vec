065d6128211c04f55e9ff41470aba6bf80f18f2b
Daniel Weibel <danielmweibel@gmail.com>
2017-12-13 23:01:44 -0500

    Add nearest 10 associations

--- app/src/main/java/com/mycompany/app/Word2VecClient.java
+++ app/src/main/java/com/mycompany/app/Word2VecClient.java
@@ -3,6 +3,7 @@ package com.mycompany.app;
 import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
 import org.deeplearning4j.models.word2vec.Word2Vec;
 import java.util.Collection;
+import java.util.Arrays;
 
 import java.net.Socket;
 import java.net.ServerSocket;
@@ -25,10 +26,24 @@ public class Word2VecClient {
           PrintWriter out = new PrintWriter(cs.getOutputStream(), true);                   
           
         ) {
-          String word;
-          while ((word = in.readLine()) != null) {
-            Collection<String> lst = vec.wordsNearest(word, 10);
-            out.println(lst); 
+          String type;
+          while ((type = in.readLine()) != null) {
+            String query = in.readLine();
+            if (type.equals("word")) {
+                Collection<String> lst = vec.wordsNearest(query, 10);
+                out.println(lst); 
+            }
+            else if (type.equals("assoc")) {
+                String tmp = query.split("::")[0];
+                String a = tmp.split(":")[0];
+                String b = tmp.split(":")[1];
+                String x = query.split("::")[1];
+                Collection<String> lst = vec.wordsNearest(Arrays.asList(b, x), Arrays.asList(a), 10);
+                out.println(lst); 
+            }
+            else {
+                out.println("Invalid command: " + type);
+            }
           }
         } catch (IOException e) {
             System.out.println("Exception caught when trying to listen on port "
